fc4f39d31c5028bd431b190c578bc284a0ca3d1e
boudedja <naila.boudedja@etu.u-paris.fr>
2024-05-10 22:01:28 +0200

    peuplement complet des tables

--- CSV/data_generator.py
+++ CSV/data_generator.py
@@ -30,25 +30,43 @@ def generate_users(n):
 def generate_films(n):
     with open(os.path.join(base_path, 'films.csv'), mode='w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(["titre", "resume", "realisation", "duree", "genre"])
-        for _ in range(n):
+        writer.writerow(["id", "titre", "resume", "realisation", "duree", "genre"])
+        for i in range(1, n+1):  # Utiliser i pour générer un ID unique pour chaque film
             titre = fake.sentence(nb_words=5)
             resume = fake.text(max_nb_chars=200)
             realisation = fake.date_this_century().isoformat()
             duree = random.randint(60, 180)
             genre = fake.word()  # Supposons que le genre correspond au nom d'un genre déjà existant
-            writer.writerow([titre, resume, realisation, duree, genre])
+            writer.writerow([i, titre, resume, realisation, duree, genre])
+
 
 def generate_publications(n):
-    with open(os.path.join(base_path, 'publications.csv'), mode='w', newline='') as file:
+    path = os.path.join(base_path, 'publications.csv')
+    with open(path, mode='w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(["auteur", "titre", "contenu", "datePublication"])
-        for _ in range(n):
-            auteur = random.randint(1, 100)  # Supposons 100 utilisateurs
-            titre = fake.sentence(nb_words=6)
-            contenu = fake.text(max_nb_chars=500)
-            datePublication = fake.date_time_this_year().isoformat()
-            writer.writerow([auteur, titre, contenu, datePublication])
+        writer.writerow(["id", "auteur", "discussionId", "titre", "contenu", "sujetId", "parentId"])
+        
+        # Les parentId valides commencent à 1 et vont jusqu'à n, mais on utilise une publication comme parent pour les suivantes
+        for i in range(1, n+1):
+            auteur = random.randint(1, 100)  # Supposer que nous avons 100 utilisateurs max
+            discussionId = random.randint(1, 50)  # Supposer que nous avons 50 discussions max
+            titre = fake.sentence()
+            contenu = fake.text()
+            sujetId = random.randint(1, 10)  # Supposer que nous avons 10 sujets max
+           # datePublication = fake.date_time_between(start_date="-1y", end_date="now").isoformat()
+            
+            # Chaque publication a un parentId, qui est un ID existant d'une publication précédente, sauf la première
+            if i == 1:
+                parentId = 2  # La première publication pourrait être de premier niveau si nécessaire
+            else:
+                parentId = random.randint(1, i-1)  # Assurer que le parentId est toujours un ID existant
+                
+            writer.writerow([i, auteur, discussionId, titre, contenu, sujetId, parentId])
+
+
+
+
+
 
 # Table 'Role'
 def generate_roles():
@@ -77,27 +95,47 @@ def generate_follows(n):
     with open(os.path.join(base_path, 'follow.csv'), mode='w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(["userId", "follower"])
-        for _ in range(n):
-            userId = random.randint(1, 100)
+        
+        unique_pairs = set()  # Set to keep track of unique (userId, follower) pairs
+        
+        while len(unique_pairs) < n:
+            userId = random.randint(1, 100)  # Assuming 100 users
             follower = random.randint(1, 100)
+            
+            # Ensure the follower is not the same as the userId
             while userId == follower:
                 follower = random.randint(1, 100)
-            writer.writerow([userId, follower])
+            
+            # Create a tuple for the pair
+            pair = (userId, follower)
+            
+            # Only write to file if the pair is unique
+            if pair not in unique_pairs:
+                writer.writerow([userId, follower])
+                unique_pairs.add(pair)  # Add the new pair to the set to ensure uniqueness
+
 
 # Table 'Serie'
-def generate_series(n):
+def generate_series(n, max_genre_id):
     with open(os.path.join(base_path, 'series.csv'), mode='w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(["id", "saison", "titre", "nbreEpisodes", "dureeParEpisode", "datePremiere", "genre"])
-        for _ in range(n):
-            id = random.randint(1, 10)
+        used_ids = set()  # Ensemble pour suivre les ID utilisés
+        while len(used_ids) < n:
+            id = random.randint(1, 200)  # Élargir la plage pour réduire les collisions
+            if id in used_ids:
+                continue  # Sauter cet ID s'il a déjà été utilisé
             saison = random.randint(1, 5)
             titre = fake.sentence(nb_words=4)
             nbreEpisodes = random.randint(8, 24)
             dureeParEpisode = random.randint(20, 60)
             datePremiere = fake.date_this_decade().isoformat()
-            genre = random.randint(1, 10)  # Supposons que les genres sont numérotés de 1 à 10
+            genre = random.randint(1, max_genre_id)
             writer.writerow([id, saison, titre, nbreEpisodes, dureeParEpisode, datePremiere, genre])
+            used_ids.add(id)  # Ajouter l'ID à l'ensemble des ID utilisés
+
+
+
 
 def generate_events(n):
     with open(os.path.join(base_path, 'events.csv'), mode='w', newline='') as file:
@@ -161,14 +199,26 @@ def generate_discussions(n):
 
 
 # Table 'ParticipationEvent'
-def generate_participation_event(n):
+def generate_participation_event(n, max_event_id):
     with open(os.path.join(base_path, 'participation_event.csv'), mode='w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(["idUser", "idEvent"])
-        for _ in range(n):
+        
+        unique_pairs = set()
+        
+        while len(unique_pairs) < n:
             idUser = random.randint(1, 100)  # Assuming 100 users
-            idEvent = random.randint(1, 20)  # Assuming 20 events
-            writer.writerow([idUser, idEvent])
+            idEvent = random.randint(1, max_event_id)  # Utiliser max_event_id pour définir le nombre max d'événements
+            
+            # Créer une paire tuple de idUser et idEvent
+            pair = (idUser, idEvent)
+            
+            # Vérifier si la paire est déjà dans l'ensemble
+            if pair not in unique_pairs:
+                writer.writerow([idUser, idEvent])
+                unique_pairs.add(pair)  # Ajouter la paire à l'ensemble pour suivre l'unicité
+
+
 
 # Table 'Archive'
 def generate_archive(n):
@@ -194,16 +244,34 @@ def generate_commentaire(n):
             writer.writerow([i, idPubli, auteur, contenu, dateCommentaire])
 
 # Table 'Reaction'
-def generate_reaction(n):
-    with open(os.path.join(base_path, 'reaction.csv'), mode='w', newline='') as file:
+
+
+def generate_reactions(n, max_pub_id):
+    path = os.path.join(base_path, 'reactions.csv')
+    with open(path, mode='w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(["idReaction", "idPubli", "idUser", "type"])
+        writer.writerow(["idPubli", "idUser", "type"])
         types = ['Like', 'Dislike', 'Neutre', 'Fun', 'Sad', 'Angry']
-        for i in range(1, n+1):
-            idPubli = random.randint(1, 100)
-            idUser = random.randint(1, 100)
-            type = random.choice(types)
-            writer.writerow([i, idPubli, idUser, type])
+        valid_pub_ids = set(range(1, max_pub_id + 1))  # Assurez-vous que max_pub_id est le maximum réel dans 'publication'
+        used_pairs = set()
+
+        while len(used_pairs) < n:
+            idPubli = random.choice(list(valid_pub_ids))
+            idUser = random.randint(1, 100)  # Supposer 100 utilisateurs max
+            pair = (idPubli, idUser)
+
+            if pair not in used_pairs:
+                type = random.choice(types)
+                writer.writerow([idPubli, idUser, type])
+                used_pairs.add(pair)
+
+
+
+
+
+
+
+
 
 # Table 'Message'
 def generate_message(n):
@@ -266,37 +334,133 @@ def generate_filme_publication(n):
             writer.writerow([i, filmId, publiId])
 
 # Table 'MotsClesPublication'
-def generate_mots_cles_publication(n):
+def generate_mots_cles_publication(n, max_publi_id, max_mot_cle_id):
+    base_path = os.path.dirname(os.path.abspath(__file__))
     with open(os.path.join(base_path, 'mots_cles_publication.csv'), mode='w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(["idPubliMotCle", "idPublication", "idMotCle"])
+        writer.writerow(["publiId", "motCleId"])
+
+        unique_pairs = set()  # Pour éviter les doublons de paires (publiId, motCleId)
+
+        while len(unique_pairs) < n:
+            publiId = random.randint(1, max_publi_id)  # Générer un publiId existant
+            motCleId = random.randint(1, max_mot_cle_id)  # Générer un motCleId existant
+
+            if (publiId, motCleId) not in unique_pairs:
+                writer.writerow([publiId, motCleId])
+                unique_pairs.add((publiId, motCleId))
+
+
+def generate_genre_cinemato(n):
+    with open(os.path.join(base_path, 'genre_cinemato.csv'), mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["id", "nom", "parentId"])
+        
+        # Example genres for illustration
+        base_genres = ['Action', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Science Fiction']
+        sub_genres = {
+            'Action': ['Adventure', 'War', 'Spy'],
+            'Comedy': ['Satire', 'Romantic', 'Slapstick'],
+            'Drama': ['Crime', 'Mystery', 'Historical'],
+            'Fantasy': ['Epic', 'Dark Fantasy', 'Urban Fantasy'],
+            'Horror': ['Gothic', 'Occult', 'Survival'],
+            'Science Fiction': ['Cyberpunk', 'Space Opera', 'Time Travel']
+        }
+        
+        # Generating base genres first
+        genres = {name: i+1 for i, name in enumerate(base_genres)}  # Mapping genre names to IDs
+        for genre, id in genres.items():
+            writer.writerow([id, genre, None])  # Top-level genres have no parent
+        
+        # Generating sub-genres
+        current_id = len(base_genres) + 1
+        for genre, sub_list in sub_genres.items():
+            parent_id = genres[genre]
+            for sub in sub_list:
+                writer.writerow([current_id, sub, parent_id])
+                current_id += 1
+def generate_event_particulier(n):
+    with open(os.path.join(base_path, 'EventParticulier.csv'), mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["id", "auteur", "nomEvent", "dateEvent", "lieuEvent", "nbPlaceDispo", "nbPlaceReserve", "organisateur", "liens_web"])
+        
         for i in range(1, n+1):
-            idPublication = random.randint(1, 100)
-            idMotCle = random.randint(1, 30)  # Assuming 30 mots clés
-            writer.writerow([i, idPublication, idMotCle])
+            id = i
+            auteur = random.randint(1, 100)  # Assuming there are 100 users
+            nomEvent = fake.sentence(nb_words=5)
+            dateEvent = fake.date_between(start_date="today", end_date="+2y").isoformat()
+            lieuEvent = fake.city()
+            nbPlaceDispo = random.randint(50, 500)
+            nbPlaceReserve = random.randint(0, nbPlaceDispo)  # Ensure this number is less than or equal to nbPlaceDispo
+            organisateur = random.randint(1, 100) if random.choice([True, False]) else None
+            
+            # Generating between 1 and 5 random web links and formatting them as a PostgreSQL array
+            liens_web = '{' + ', '.join([fake.url() for _ in range(random.randint(1, 5))]) + '}'
+
+            writer.writerow([id, auteur, nomEvent, dateEvent, lieuEvent, nbPlaceDispo, nbPlaceReserve, organisateur, liens_web])
+
+def generate_interesse_event(n, max_user_id, max_event_id):
+    with open(os.path.join(base_path, 'InteresseEvent.csv'), mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["userId", "eventId"])
+        
+        unique_pairs = set()  # To avoid duplicate (userId, eventId) pairs
+
+        while len(unique_pairs) < n:
+            userId = random.randint(1, max_user_id)  # Assuming max_user_id is the maximum number of users
+            eventId = random.randint(1, max_event_id)  # Assuming max_event_id is the maximum number of events
+
+            # Ensure the pair is unique before writing
+            if (userId, eventId) not in unique_pairs:
+                writer.writerow([userId, eventId])
+                unique_pairs.add((userId, eventId))
+
+def generate_historique_publication(n, max_user_id, max_publi_id):
+    with open(os.path.join(base_path, 'historique_publication.csv'), mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["idUser", "idPubli", "action", "dateAction"])
+        
+        actions = ["ajouter", "voir", "repondre à une publication"]  # Définir les types d'actions possibles
+
+        for _ in range(n):
+            idUser = random.randint(1, max_user_id)  # Choix d'un idUser existant
+            idPubli = random.randint(1, max_publi_id)  # Choix d'un idPubli existant
+            action = random.choice(actions)  # Sélection aléatoire d'une action
+            dateAction = fake.date_between(start_date="-2y", end_date="today")  # Génération d'une date d'action
+
+            writer.writerow([idUser, idPubli, action, dateAction])
 
 # activation de ses fonctions
-            
+generate_genre_cinemato(24)  # Générer des genres cinématographiques, en assurant qu'il y en a assez pour les références
+
 generate_users(100)  # Générer 100 utilisateurs
 generate_films(50)   # Générer 50 films
 generate_publications(100)  # Générer 100 publications
 
-generate_sujet_publication(10)  # Generate 10 sujet publication entries
-generate_mots_cles(30)          # Generate 30 unique mots clés
-generate_categorie_discussion(10)  # Generate 10 unique categories
-generate_discussions(50)        # Generate 50 discussions
+generate_sujet_publication(10)  # Générer 10 entrées de sujet de publication
+generate_mots_cles(30)          # Générer 30 mots clés uniques
+generate_categorie_discussion(10)  # Générer 10 catégories de discussion uniques
+generate_discussions(50)        # Générer 50 discussions
 
-generate_participation_event(50)  # Generate 50 event participations
-generate_archive(30)              # Generate 30 archive entries
-generate_commentaire(100)         # Generate 100 comments
-generate_reaction(200)            # Generate 200 reactions
-generate_message(150)             # Generate 150 messages
-generate_notification(80)         # Generate 80 notifications
+generate_participation_event(50, 20)  # Générer 50 participations à des événements, avec un maximum de 20 événements différents
+generate_archive(30)              # Générer 30 entrées d'archive
+generate_commentaire(100)         # Générer 100 commentaires
+# Exemple de correction de l'appel de la fonction
+generate_reactions(200, 100)  # Générer 200 réactions pour 100 publications
+
+generate_message(150)             # Générer 150 messages
+generate_notification(80)         # Générer 80 notifications
 
 generate_roles()
 generate_amis(100)
 generate_follows(100)
 generate_filme_publication(100)
-generate_mots_cles_publication(100)
-generate_series(100)
+generate_mots_cles_publication(100, 100, 30)  # Supposons que vous ayez 100 publications et 30 mots clés
+
+generate_series(100, 24)  # Générer 100 séries en s'assurant que les genres existent
+generate_event_particulier(50)  # Generate 50 particular events
+generate_interesse_event(200, 100, 50)  # Generate 200 entries with up to 100 users and 50 events
+generate_historique_publication(100, 100, 50)  # Générer 100 entrées historiques avec jusqu'à 100 utilisateurs et 50 publications
+
+
 
--- tables.sql
+++ tables.sql
@@ -23,6 +23,9 @@ DROP TABLE IF EXISTS CategorieDiscussion;
 DROP TABLE IF EXISTS Message;
 DROP TABLE IF EXISTS Users;
 
+DROP TYPE IF EXISTS TypeRole CASCADE;
+DROP TYPE IF EXISTS TypeReaction CASCADE;
+
 
 
 --  _id INTEGER REFERENCES Role(id) ON DELETE SET NULL/enuuuum c'est comme on veut 
@@ -123,14 +126,14 @@ CREATE TABLE Discussion(
 
 
 CREATE TABLE Publication(
-    id SERIAL PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     auteur INTEGER REFERENCES Users(id),
     discussionId INTEGER REFERENCES Discussion(id),
-    titre VARCHAR(100) NOT NULL,
+    titre VARCHAR(200) NOT NULL,
     contenu TEXT NOT NULL,
     sujetId INTEGER REFERENCES SujetPublication(id),
     -- motsCles INTEGER REFERENCES MotsCles(motCleId),
-    datePublication TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    --datePublication TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     parentId  INTEGER REFERENCES Publication(id), --NULL si publication premier niveau
     FOREIGN KEY (auteur) REFERENCES Users(id) ON DELETE CASCADE
 );
@@ -254,26 +257,30 @@ CREATE TABLE HistoriquePublication(
 
 
 -- Peuplement de la base de données à partir des fichiers CSV --
+\copy GenreCinemato(id, nom, parentId) FROM 'CSV/genre_cinemato.csv' WITH (FORMAT csv, HEADER true);
 \copy Users(username, password, email, role) FROM 'CSV/Utilisateurs.csv' WITH (FORMAT csv, HEADER true);
 \copy Amis(user1, user2) FROM 'CSV/amis.csv' WITH (FORMAT csv, HEADER true);
 \copy Follower(id, folower) FROM 'CSV/follow.csv' WITH (FORMAT csv, HEADER true);
-\copy Archive(idArchive, dateArchivage, raison, idEvent) FROM 'CSV/archive.csv' WITH (FORMAT csv, HEADER true);
 \copy Film(id, titre, resume, realisation, duree, genre) FROM 'CSV/films.csv' WITH (FORMAT csv, HEADER true);
 \copy Serie(id, saison, titre, nbreEpisodes, dureeParEpisode, datePremiere, genre) FROM 'CSV/series.csv' WITH (FORMAT csv, HEADER true);
-\copy Reaction(idPubli, idUser, type) FROM 'CSV/reaction.csv' WITH (FORMAT csv, HEADER true);
 \copy MotsCles(motCleId, motCle) FROM 'CSV/mots_cles.csv' WITH (FORMAT csv, HEADER true);
-\copy MotsClesPublication(publiId, motCleId) FROM 'CSV/mots_cles_publication.csv' WITH (FORMAT csv, HEADER true);
-\copy ParticipationEvent(userId, eventId) FROM 'CSV/participation_event.csv' WITH (FORMAT csv, HEADER true);
-\copy InteresseEvent(userId, eventId) FROM 'CSV/interesse_event.csv' WITH (FORMAT csv, HEADER true);
-\copy EventParticulier(id, auteur, nomEvent, dateEvent, lieuEvent, nbPlaceDispo, nbPlaceReserve, organisateur, liens_web) FROM 'CSV/events.csv' WITH (FORMAT csv, HEADER true);
-\copy Publication(id, auteur, discussionId, titre, contenu, sujetId, datePublication, parentId) FROM 'CSV/publications.csv' WITH (FORMAT csv, HEADER true);
-\copy SujetPublication(id, description) FROM 'CSV/sujet_publication.csv' WITH (FORMAT csv, HEADER true);
 \copy CategorieDiscussion(id, nomCategorie) FROM 'CSV/categorie_discussion.csv' WITH (FORMAT csv, HEADER true);
+\copy SujetPublication(id, description) FROM 'CSV/sujet_publication.csv' WITH (FORMAT csv, HEADER true);
+
+\copy EventParticulier(id, auteur, nomEvent, dateEvent, lieuEvent, nbPlaceDispo, nbPlaceReserve, organisateur, liens_web) FROM 'CSV/EventParticulier.csv' WITH (FORMAT csv, HEADER true);
+\copy InteresseEvent(userId, eventId) FROM 'CSV/InteresseEvent.csv' WITH (FORMAT csv, HEADER true);
+\copy ParticipationEvent(userId, eventId) FROM 'CSV/participation_event.csv' WITH (FORMAT csv, HEADER true);
 \copy Discussion(id, auteur, titre, description, categorie) FROM 'CSV/discussions.csv' WITH (FORMAT csv, HEADER true);
+\copy Publication(id, auteur, discussionId, titre, contenu, sujetId, parentId) FROM 'CSV/publications.csv' WITH (FORMAT csv, HEADER true);
+\copy MotsClesPublication(publiId, motCleId) FROM 'CSV/mots_cles_publication.csv' WITH (FORMAT csv, HEADER true);
+
+\copy Reaction(idPubli, idUser, type) FROM 'CSV/reactions.csv' WITH (FORMAT csv, HEADER true);
+\copy Archive(idArchive, dateArchivage, raison, idEvent) FROM 'CSV/archive.csv' WITH (FORMAT csv, HEADER true);
 \copy Message(id, expéditeur, destinataire, contenu) FROM 'CSV/message.csv' WITH (FORMAT csv, HEADER true);
-\copy Commentaire(idCommentaire, idPubli, auteur, contenu, dateCommentaire) FROM 'CSV/commentaire.csv' WITH (FORMAT csv, HEADER true);
-\copy Notification(id, notificationType, vue, dateEnvoie, publication, expéditeur, destinataire) FROM 'CSV/notification.csv' WITH (FORMAT csv, HEADER true);
+--\copy Commentaire(idCommentaire, idPubli, auteur, contenu, dateCommentaire) FROM 'CSV/commentaire.csv' WITH (FORMAT csv, HEADER true);
+--\copy Notification(id, notificationType, vue, dateEnvoie, publication, expéditeur, destinataire) FROM 'CSV/notification.csv' WITH (FORMAT csv, HEADER true);
 \copy HistoriquePublication(idUser, idPubli, action, dateAction) FROM 'CSV/historique_publication.csv' WITH (FORMAT csv, HEADER true);
+
 
 -- Assurez-vous que les noms de fichier et les chemins correspondent à ceux de votre système de fichiers.
 
